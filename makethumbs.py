a0ab1d40cbd9219b552a649d27fbfdee3a7dfbfa
Danil <d.kabirov@innopolis.ru>
2019-11-18 02:51:46 +0300

    portfolios added

--- makethumbs.py
+++ makethumbs.py
@@ -1,7 +1,13 @@
 import os
 from PIL import Image
 
-for file in os.listdir('images/fulls'):
-    image = Image.open('images/fulls/' + file)
-    image.thumbnail((512,512), Image.ANTIALIAS)
-    image.save('images/thumbs/' + file, "JPEG")
+for dir in os.listdir():
+    if os.path.exists(dir + '/images') and (not os.path.exists(dir + '/images/thumbs') or len(os.listdir(dir + '/images/thumbs')) == 0):
+        if not os.path.exists(dir + '/images/thumbs'):
+            os.mkdir(dir + '/images/thumbs')
+        for file in os.listdir(dir + '/images/fulls'):
+            image = Image.open(dir + '/images/fulls/' + file)
+            image.thumbnail((512,512), Image.ANTIALIAS)
+            image.save(dir + '/images/thumbs/' + file, "JPEG")
+
+print('Done!')
