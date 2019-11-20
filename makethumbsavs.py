d5949cdfee3e4d971c3887efc052d9ee566df3d5
Danil <d.kabirov@innopolis.ru>
2019-11-21 00:15:20 +0300

    portfolios added

--- makethumbsavs.py
+++ makethumbsavs.py
@@ -4,18 +4,17 @@ from PIL import Image
 for dir in os.listdir():
     if os.path.exists(dir + '/images'):
         for file in os.listdir(dir):
-            if (file[-4:] == '.jpg' or file[-4:] == '.png') and file.find('thumb') == -1:
-                print('ok')
-                try:
-                    os.rename(dir + '/' + dir, dir + '/' + dir + '_thumb')
-                except:
-                    pass
-                #print(dir + '/' + file + ' renamed to ' + dir + '/' + dir + '/' + file)
-                #os.rename(dir + '/' + file, dir + '/' + dir + '/' + file)
-                #print(dir + '/' + file[:-4] + '_thumb' + file[-4:] + ' renamed to ' + dir + '/' + file)
-                #os.rename(dir + '/' + file[:-4] + '_thumb' + file[-4:], dir + '/' + file)
-                #image = Image.open(dir + '/' + file)
-                #image.thumbnail((512,512), Image.ANTIALIAS)
-                #image.save(dir + '/' + file[:-4] + '_thumb' + file[-4:], "JPEG")
+            if (file[-4:] == '.jpg' or file[-4:] == '.png') and not os.path.exists(dir + '/' + dir + '_full/' + file):
+                #print('ok')
+                #try:
+                #    os.rename(dir + '/' + dir + '_thumb', dir + '/' + dir + '_full')
+                #except:
+                #    pass
+                os.mkdir(dir + '/' + dir + '_full')
+                print(dir + '/' + file + ' renamed to ' + dir + '_full/' + dir + '/' + file)
+                os.rename(dir + '/' + file, dir + '/' + dir + '_full/' + file)
+                image = Image.open(dir + '/' + dir + '_full/' + file)
+                image.thumbnail((512,512), Image.ANTIALIAS)
+                image.save(dir + '/' + file, "JPEG")
 
 print('Done!')
